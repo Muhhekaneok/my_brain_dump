ca4f2fecf581f514c83fecc2b9281dd8510e7213
wizardrad@gmail.com <wizardrad@gmail.com>
2025-04-05 13:38:25 +0500

    add second function to factorial_calculator.py

--- src/factorial_calculator.py
+++ src/factorial_calculator.py
@@ -1,12 +1,21 @@
-def get_factorial(n: int) -> int:
+def get_factorial_recursive(n: int) -> int:
     if not isinstance(n, int):
         raise TypeError("Input must be an integer")
     if n < 0:
-        raise TypeError("Input must be a non-negative number")
+        raise ValueError("Input must be a non-negative number")
 
     if n <= 1:
         return 1
-    return get_factorial(n - 1) * n
+    return get_factorial_recursive(n - 1) * n
 
 
-print(get_factorial(-5))
+def get_factorial_iterative(n: int) -> int:
+    if not isinstance(n, int):
+        raise TypeError("Input must be an integer")
+    if n < 0:
+        raise ValueError("Input must be non negative number")
+
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
--- tests/test_get_factorial.py
+++ tests/test_get_factorial.py
@@ -1,22 +1,23 @@
 import unittest
-from src.factorial_calculator import get_factorial
+from src.factorial_calculator import get_factorial_recursive, get_factorial_iterative
 
-class TestGetFactorial(unittest.TestCase):
+
+class TestGetFactorialRecursive(unittest.TestCase):
     def test_base_cases(self):
-        self.assertEqual(get_factorial(0), 1)
-        self.assertEqual(get_factorial(1), 1)
+        self.assertEqual(get_factorial_recursive(0), 1)
+        self.assertEqual(get_factorial_recursive(1), 1)
 
     def test_recursive_cases(self):
-        self.assertEqual(get_factorial(3), 6)
-        self.assertEqual(get_factorial(5), 120)
-        self.assertEqual(get_factorial(7), 5040)
+        self.assertEqual(get_factorial_recursive(3), 6)
+        self.assertEqual(get_factorial_recursive(5), 120)
+        self.assertEqual(get_factorial_recursive(7), 5040)
 
     def test_type_error(self):
         with self.assertRaises(TypeError):
-            get_factorial("5")
+            get_factorial_recursive("5")
         with self.assertRaises(TypeError):
-            get_factorial(3.4)
+            get_factorial_recursive(3.4)
 
     def test_negative_value(self):
         with self.assertRaises(ValueError):
-            get_factorial(-7)
+            get_factorial_recursive(-7)

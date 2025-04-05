1c4cc1d2b6c49e8df376b523dc6772f9f2373a62
wizardrad@gmail.com <wizardrad@gmail.com>
2025-04-05 19:12:48 +0500

    add second unittest for factorial_calculator.py

--- src/factorial_calculator.py
+++ src/factorial_calculator.py
@@ -13,7 +13,7 @@ def get_factorial_iterative(n: int) -> int:
     if not isinstance(n, int):
         raise TypeError("Input must be an integer")
     if n < 0:
-        raise ValueError("Input must be non negative number")
+        raise ValueError("Input must be non-negative number")
 
     result = 1
     for i in range(2, n + 1):
--- tests/test_get_factorial.py
+++ tests/test_get_factorial.py
@@ -21,3 +21,24 @@ class TestGetFactorialRecursive(unittest.TestCase):
     def test_negative_value(self):
         with self.assertRaises(ValueError):
             get_factorial_recursive(-7)
+
+
+class TestGetFactorialIterative(unittest.TestCase):
+    def test_base_cases(self):
+        self.assertEqual(get_factorial_iterative(0), 1)
+        self.assertEqual(get_factorial_iterative(1), 1)
+
+    def test_iterative_cases(self):
+        self.assertEqual(get_factorial_iterative(3), 6)
+        self.assertEqual(get_factorial_iterative(5), 120)
+        self.assertEqual(get_factorial_iterative(7), 5040)
+
+    def test_type_error(self):
+        with self.assertRaises(TypeError):
+            get_factorial_iterative("1")
+        with self.assertRaises(TypeError):
+            get_factorial_iterative(7.8)
+
+    def test_negative_value(self):
+        with self.assertRaises(ValueError):
+            get_factorial_iterative(-1)

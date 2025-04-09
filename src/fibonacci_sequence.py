298470022ba66b2d81e3ac4c8a7c6e32e089ff73
wizardrad@gmail.com <wizardrad@gmail.com>
2025-04-09 18:32:34 +0500

    modified fibonacci function

--- src/fibonacci_sequence.py
+++ src/fibonacci_sequence.py
@@ -1,8 +1,15 @@
 def get_fibonacci_seq(n: int) -> list:
     if not isinstance(n, int):
         raise TypeError("Input must be an integer")
+    if n < 0:
+        raise ValueError("Input must be a non-negative number")
+
+    if n == 0:
+        return []
+    if n == 1:
+        return [0]
 
     fibo = [0, 1]
-    for _ in range(2, n + 1):
+    for _ in range(2, n):
         fibo.append(fibo[-1] + fibo[-2])
     return fibo

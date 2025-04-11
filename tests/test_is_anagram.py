c0e41918a731d2bf0e9100552a79b126e7dd8a46
wizardrad@gmail.com <wizardrad@gmail.com>
2025-04-11 13:50:47 +0500

    add unittest for function with one dict

--- tests/test_is_anagram.py
+++ tests/test_is_anagram.py
@@ -1,9 +1,11 @@
 import unittest
-from src.anagram_checker import is_anagram_2_dicts
+from src.anagram_checker import is_anagram_2_dicts, is_anagram_1_dict
+
 
 class TestIsAnagramTwoDicts(unittest.TestCase):
     def test_anagram(self):
         self.assertEqual(is_anagram_2_dicts("silent", "listen"), True)
+        self.assertEqual(is_anagram_2_dicts("Silent", "LISTEN"), True)
         self.assertEqual(is_anagram_2_dicts("cheater", "teacher"), True)
         self.assertEqual(is_anagram_2_dicts("a gentleman", "elegant man"), True)
         self.assertEqual(is_anagram_2_dicts("forty five", "over fifty"), True)
@@ -24,3 +26,28 @@ class TestIsAnagramTwoDicts(unittest.TestCase):
             is_anagram_2_dicts([], "b")
         with self.assertRaises(TypeError):
             is_anagram_2_dicts([], ())
+
+
+class TestIsAnagramOneDict(unittest.TestCase):
+    def test_anagram(self):
+        self.assertEqual(is_anagram_1_dict("Silent", "SILENT"), True)
+        self.assertEqual(is_anagram_1_dict("cheater", "teacher"), True)
+        self.assertEqual(is_anagram_1_dict("a gentleman", "elegant man"), True)
+        self.assertEqual(is_anagram_1_dict("forty five", "over fifty"), True)
+        self.assertEqual(is_anagram_2_dicts("coronavirus", "carnivorous"), True)
+
+    def test_empty_lines(self):
+        self.assertEqual(is_anagram_1_dict("", ""), True)
+
+    def test_not_anagram(self):
+        self.assertEqual(is_anagram_1_dict("new your times", "monkeys write"), False)
+        self.assertEqual(is_anagram_1_dict("funeral", "real fun"), False)
+        self.assertEqual(is_anagram_1_dict(" ", ""), False)
+
+    def test_type_error(self):
+        with self.assertRaises(TypeError):
+            is_anagram_1_dict(1, "a")
+        with self.assertRaises(TypeError):
+            is_anagram_1_dict("x", [])
+        with self.assertRaises(TypeError):
+            is_anagram_1_dict([], ())

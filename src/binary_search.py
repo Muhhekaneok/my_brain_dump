42f7fd086349cb3975ceaeb0a889e31c60f5a8d2
wizardrad@gmail.com <wizardrad@gmail.com>
2025-04-11 22:42:14 +0500

    add binary search function

---
+++ src/binary_search.py
@@ -0,0 +1,17 @@
+def get_binary_search(array: list, target: int) -> int:
+    if not isinstance(array, list) or not isinstance(target, int):
+        raise TypeError("Inputs must be list and integer")
+
+    left, right = 0, len(array) - 1
+    array.sort()
+
+    while left <= right:
+        middle = (left + right) // 2
+        if array[middle] == target:
+            return middle
+        elif middle < target:
+            left = middle + 1
+        else:
+            right = middle - 1
+
+    return -1

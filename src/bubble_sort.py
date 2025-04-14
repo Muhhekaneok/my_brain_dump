cb8dd56218712e259ea3ac19af8e46caafbe1d13
wizardrad@gmail.com <wizardrad@gmail.com>
2025-04-14 13:33:22 +0500

    get_bubble_sort_optimized function was added

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -10,3 +10,21 @@ def get_bubble_sort(array: list) -> list:
                 array[j], array[j + 1] = array[j + 1], array[j]
 
     return array
+
+
+def get_bubble_sort_optimized(array) -> list:
+    if not isinstance(array, list):
+        raise TypeError("Input must be a list")
+
+    n = len(array)
+    for i in range(n):
+        swapped = False
+        for j in range(n - 1 - i):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return array

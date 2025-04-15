2bb925f42ff7ac25f353ade138adb64281405aa6
wizardrad@gmail.com <wizardrad@gmail.com>
2025-04-15 10:35:08 +0500

    unittest for get_bubble_sort_optimized

--- tests/test_bubble_sort.py
+++ tests/test_bubble_sort.py
@@ -1,5 +1,5 @@
 import unittest
-from src.bubble_sort import get_bubble_sort
+from src.bubble_sort import get_bubble_sort, get_bubble_sort_optimized
 
 
 class TestBubbleSortFullAction(unittest.TestCase):
@@ -32,3 +32,32 @@ class TestBubbleSortFullAction(unittest.TestCase):
             get_bubble_sort("3")
         with self.assertRaises(TypeError):
             get_bubble_sort(10)
+
+
+class TestBubbleSortOptimized(unittest.TestCase):
+    def test_unsorted_mix_numbers(self):
+        self.assertEqual(
+            get_bubble_sort_optimized([2, 6, 1, 0, 4, -1, 5, 3]), [-1, 0, 1, 2, 3, 4, 5, 6])
+
+    def test_already_sorted(self):
+        self.assertEqual(get_bubble_sort_optimized([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+
+    def test_negative(self):
+        self.assertEqual(
+            get_bubble_sort_optimized([-7, -2, -3, -5, -1, -6, -4]), [-7, -6, -5, -4, -3, -2, -1])
+
+    def test_single_element(self):
+        self.assertEqual(get_bubble_sort_optimized([8]), [8])
+
+    def test_duplicates(self):
+        self.assertEqual(get_bubble_sort_optimized([1, 1, 1]), [1, 1, 1])
+
+    def test_incorrect_elements(self):
+        with self.assertRaises(TypeError):
+            get_bubble_sort_optimized([1, 2, "3"])
+
+    def test_type_error(self):
+        with self.assertRaises(TypeError):
+            get_bubble_sort_optimized("5")
+        with self.assertRaises(TypeError):
+            get_bubble_sort_optimized(99)

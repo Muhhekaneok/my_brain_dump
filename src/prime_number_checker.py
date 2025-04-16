69ceb8519e2b51e5ff2afaf2b927cd63bd738f49
wizardrad@gmail.com <wizardrad@gmail.com>
2025-04-16 22:47:39 +0500

    modified code and unittests

--- src/prime_number_checker.py
+++ src/prime_number_checker.py
@@ -1,3 +1,6 @@
+import math
+
+
 def is_prime_number(x: int) -> bool:
     if not isinstance(x, int):
         raise TypeError("Input must be an integer")
@@ -6,6 +9,19 @@ def is_prime_number(x: int) -> bool:
         raise ValueError("Input must be 2 or greater")
 
     for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
+
+
+def is_prime_number_optimized(x: int) -> bool:
+    if not isinstance(x, int):
+        raise TypeError("Input must be an integer")
+
+    if x < 2:
+        raise ValueError("Input must be 2 or greater")
+
+    for i in range(2, int(math.sqrt(x)) + 1):
         if x % i == 0:
             return False
     return True
--- tests/test_prime_number.py
+++ tests/test_prime_number.py
@@ -1,23 +1,24 @@
 import unittest
-from src.prime_number_checker import is_prime_number
+
+from src.prime_number_checker import is_prime_number, is_prime_number_optimized
 
 
 class TestPrimeNumberChecker(unittest.TestCase):
     def test_is_prime(self):
-        self.assertEqual(is_prime_number(2), True)
-        self.assertEqual(is_prime_number(3), True)
-        self.assertEqual(is_prime_number(5), True)
-        self.assertEqual(is_prime_number(7), True)
-        self.assertEqual(is_prime_number(11), True)
-        self.assertEqual(is_prime_number(13), True)
-        self.assertEqual(is_prime_number(997), True)
+        self.assertTrue(is_prime_number(2))
+        self.assertTrue(is_prime_number(3))
+        self.assertTrue(is_prime_number(5))
+        self.assertTrue(is_prime_number(7))
+        self.assertTrue(is_prime_number(11))
+        self.assertTrue(is_prime_number(13))
+        self.assertTrue(is_prime_number(997))
 
     def test_is_not_prime(self):
-        self.assertEqual(is_prime_number(4), False)
-        self.assertEqual(is_prime_number(6), False)
-        self.assertEqual(is_prime_number(9), False)
-        self.assertEqual(is_prime_number(15), False)
-        self.assertEqual(is_prime_number(999), False)
+        self.assertFalse(is_prime_number(4))
+        self.assertFalse(is_prime_number(6))
+        self.assertFalse(is_prime_number(9))
+        self.assertFalse(is_prime_number(15))
+        self.assertFalse(is_prime_number(999))
 
     def test_negative_value(self):
         with self.assertRaises(ValueError):
@@ -28,3 +29,31 @@ class TestPrimeNumberChecker(unittest.TestCase):
             is_prime_number("a")
         with self.assertRaises(TypeError):
             is_prime_number([10])
+
+
+class TestPrimeNumberCheckerOptimized(unittest.TestCase):
+    def test_is_prime(self):
+        self.assertTrue(is_prime_number_optimized(2))
+        self.assertTrue(is_prime_number_optimized(3))
+        self.assertTrue(is_prime_number_optimized(5))
+        self.assertTrue(is_prime_number_optimized(7))
+        self.assertTrue(is_prime_number_optimized(11))
+        self.assertTrue(is_prime_number_optimized(13))
+        self.assertTrue(is_prime_number_optimized(997))
+
+    def test_is_not_prime(self):
+        self.assertFalse(is_prime_number_optimized(4))
+        self.assertFalse(is_prime_number_optimized(6))
+        self.assertFalse(is_prime_number_optimized(9))
+        self.assertFalse(is_prime_number_optimized(15))
+        self.assertFalse(is_prime_number_optimized(999))
+
+    def test_negative_value(self):
+        with self.assertRaises(ValueError):
+            is_prime_number_optimized(-4)
+
+    def test_type_error(self):
+        with self.assertRaises(TypeError):
+            is_prime_number_optimized("a")
+        with self.assertRaises(TypeError):
+            is_prime_number_optimized([10])

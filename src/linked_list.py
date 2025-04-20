30a9714495751dd71b56a9409f50b0a1a04bfded
wizardrad@gmail.com <wizardrad@gmail.com>
2025-04-20 20:40:54 +0500

    linked list

---
+++ src/linked_list.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        tail = self.head
+        while tail.next:
+            tail = tail.next
+        tail.next = new_node
+
+    def delete(self, data):
+        current = self.head
+        prev = None
+        while current:
+            if current.data == data:
+                if prev:
+                    prev.next = current.next
+                else:
+                    self.head = current.next
+                return
+            prev = current
+            current = current.next
+
+    def find(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return current
+            current = current.next
+        return None
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
